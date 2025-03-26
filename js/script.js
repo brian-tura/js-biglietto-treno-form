5dae2551f73ccc234a7fe2cb5c213bdc5f79eee1
brian-tura <b.tura2002@gmail.com>
2025-03-26 16:23:27 +0100

    Added name and surname

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
 const btn = document.getElementById('btn');
 const price = 0.21;
+const showName = document.getElementById('nome');
 
-btn.addEventListener("click", function(event){
+
+btn.addEventListener("click", function (event) {
     event.preventDefault();
     const distance = document.getElementById('distance').value;
     const age = document.getElementById('age').value;
+    const name = document.getElementById('name');
+    showName.innerHTML = name.value.trim();
     let finalPrice = price * distance;
-    if(age < 18){
+    if (age < 18) {
         finalPrice *= 0.8;
     }
-    if(age > 65){
+    if (age > 65) {
         finalPrice *= 0.6;
     }
     console.log(finalPrice);
-})
+});
 
 
 
